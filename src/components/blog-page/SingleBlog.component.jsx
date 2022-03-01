d5ac8bde75a13417cae7cd37830f13bfb9360006
nilaw2017 <nilaw.manandhar2016@gmail.com>
2022-03-01 10:22:52 +0545

    Need to fix singleBlog Image

--- src/components/blog-page/SingleBlog.component.jsx
+++ src/components/blog-page/SingleBlog.component.jsx
@@ -8,7 +8,7 @@ const SingleBlog = ()=> {
     useEffect(()=> {
         const fetchBlogData = async ()=> {
             // console.log('params',params)
-            const res = await fetch(`http://localhost:1337/api/blogs?filters[BlogID][$eq]=${params.blogId}&populate=*`)
+            const res = fetch(`http://localhost:1337/api/blogs?filters[BlogID][$eq]=${params.blogId}&populate=*`)
             const data = await res.json()
     
             console.log('data >>',data.data[0].attributes)
@@ -19,8 +19,7 @@ const SingleBlog = ()=> {
 
     return (
         <section className="container text-center">
-
-            <img src={`http://localhost:1337${blog.FeaturedImage.data.attributes.url}`} />
+            {/* <img src={`http://localhost:1337${blog.FeaturedImage.data.attributes.url}`} /> */}
             <h1>MainDivision = {blog.BlogTitle}</h1>
             <h3>Subdivision = {blog.BlogBody}</h3>
         </section>
