2243679c51b728272e14581bfda14a757a70f5b3
nilaw2017 <nilaw.manandhar2016@gmail.com>
2022-03-01 22:54:52 +0545

    SingleBlog image loading FIXED

--- src/components/blog-page/SingleBlog.component.jsx
+++ src/components/blog-page/SingleBlog.component.jsx
@@ -8,18 +8,24 @@ const SingleBlog = ()=> {
     useEffect(()=> {
         const fetchBlogData = async ()=> {
             // console.log('params',params)
-            const res = fetch(`http://localhost:1337/api/blogs?filters[BlogID][$eq]=${params.blogId}&populate=*`)
-            const data = await res.json()
-    
-            console.log('data >>',data.data[0].attributes)
-            setBlog(data.data[0].attributes)
+            fetch(`http://localhost:1337/api/blogs?populate=*&filters[BlogID][$eq]=${params.blogId}`)
+            .then((res) => {
+                if(res.status >= 400) {
+                    console.log("Bad Response")
+                }
+                return res.json();
+            })
+            .then((data)=> {
+                setBlog(data.data[0].attributes)
+            })    
         }
         fetchBlogData()
     },[])
-
+    const image = blog.FeaturedImage == undefined?blog:blog.FeaturedImage.data.attributes.url;
+    console.log("TEST >>", image)
     return (
         <section className="container text-center">
-            {/* <img src={`http://localhost:1337${blog.FeaturedImage.data.attributes.url}`} /> */}
+            <img src={`http://localhost:1337${image}`} />
             <h1>MainDivision = {blog.BlogTitle}</h1>
             <h3>Subdivision = {blog.BlogBody}</h3>
         </section>
